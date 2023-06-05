3cd70e4aa12e688b2933d44d85f4bf125c9fd24a
DarielBR <dbombinorevuelta@gmail.com>
2023-06-05 13:51:04 +0200

    MainActivity class cleansing.

--- app/src/main/java/com/example/myplayground/MainActivity.kt
+++ app/src/main/java/com/example/myplayground/MainActivity.kt
@@ -9,21 +9,12 @@ import com.example.myplayground.challenges.codility.lessons.frogRiverOne
 import com.example.myplayground.challenges.codility.lessons.maxCounters
 import com.example.myplayground.challenges.codility.lessons.missingInteger
 import com.example.myplayground.challenges.codility.lessons.permCheck
+import com.example.myplayground.challenges.codility.tests.simple
 
 import com.example.myplayground.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    var entries = arrayOf("arrb6???4xxbl5???eee5",
-                                "aa6?9",
-                                "acc?7??sss?3rr1??????5",
-                                "aaaaaaarrrrr??????",
-                                "9???1???9???1???9",
-                                "9???1???9??1???9",
-                                "4?5?4?aamm9",
-                                "5??aaaaaaaaaaaaaaaaaaa?5?5",
-                                "5??aaaaaaaaaaaaaaaaaaa?5?a??5",
-                                "mbbv???????????4??????ddsdsdcc9?")
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,16 +23,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val textView: TextView = binding.textView
-        val powersOfTwo: MutableList<Int> = mutableListOf()
-        var i = 0
-        val two = 2
-        while(i < 100){
-            powersOfTwo.add(Math.pow(2.0,i.toDouble()).toInt())
-            powersOfTwo.add(Math.pow(2.0,i.toDouble()).toInt())
-            powersOfTwo.add(Math.pow(2.0,i.toDouble()).toInt())
-            powersOfTwo.add(Math.pow(2.0,i.toDouble()).toInt())
-            i++
+
+        for(count in 1..1000){
+            simple(count)
         }
+
         textView.text = missingInteger(intArrayOf(-1000000,1000000)).toString()
     }
 
---
+++ app/src/main/java/com/example/myplayground/challenges/codility/tests/OpenbankSL.kt
@@ -0,0 +1,25 @@
+package com.example.myplayground.challenges.codility.tests
+
+inline fun simple(x: Int): Int{return x*x}
+
+fun getUserName(userId: Int): String{
+    val userDao = UserDao()
+    val user = userDao.getUserById(userId)
+    return user.name
+}
+
+class Repository (
+    val apiRetrofit: ApiRetrofit,
+    val bdRoom: DbRoom
+        ){
+    fun fetchData(): Result{
+        val localRsult = bdRoom.fetchData()
+        return if(localRsult != null){
+            localRsult
+        }else {
+            apiRetrofit.fetchData().also {
+                bdRoom.saveData(remoteResult)
+            }
+        }
+    }
+}
