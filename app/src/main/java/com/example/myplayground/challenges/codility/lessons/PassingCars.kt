f2a7c8a9d4a6558b7a2d3232285ffd65ede828e6
DarielBR <dbombinorevuelta@gmail.com>
2023-07-17 18:44:51 +0200

    Lesson 5 PassingCars exercise added.

---
+++ app/src/main/java/com/example/myplayground/challenges/codility/lessons/PassingCars.kt
@@ -0,0 +1,20 @@
+package com.example.myplayground.challenges.codility.lessons
+
+fun passingCars(A: IntArray): Int {
+    var goingEast = 0 // count of zeros (cars traveling east)
+    var crossingEastWest = 0 // count of passing car pairs
+
+    for (i in A.indices) {
+        if (A[i] == 0) {
+            goingEast++ // increment the count of zeros
+        } else {
+            crossingEastWest += goingEast // add the count of zeros to the count of pairs
+        }
+
+        if (crossingEastWest > 1_000_000_000) {
+            return -1 // check if the count of pairs exceeds the limit
+        }
+    }
+
+    return crossingEastWest
+}
